e06022f64046fcf7951d04bb8dc234b6200c3892
KitsonBroadhurst <kitson.broadhurst@gmail.com>
2023-09-06 10:29:51 +0100

    add try/catch to api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,8 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default async function handler(req, res) {
-  const { time } = JSON.parse(req.body);
-  console.log("starint API, waiting: ", time, " seconds");
-  await new Promise((resolve) => setTimeout(resolve, time * 100)); // wait time seconds
-  res.status(200).json({ success: true });
+  try {
+    const { time } = JSON.parse(req.body);
+    console.log("starint API, waiting: ", time, " seconds");
+    await new Promise((resolve) => setTimeout(resolve, time * 100)); // wait time seconds
+    res.status(200).json({ success: true });
+  } catch (error) {
+    console.log("error is ", error);
+    res.status(500).json({ error: error?.message || "Server error in hello" });
+  }
 }
